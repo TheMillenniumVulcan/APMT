e6fdd4d31496f9ab41090cd000997c8746b7a8b3
kl4m <manas.pradhan.kl4m@statefarm.com>
2015-04-21 17:57:29 -0700

    Added tickets to the storyboard mockup Added the sort, group and filter options

--- storyboard.js
+++ storyboard.js
@@ -1,4 +1,9 @@
 $('#myTabs a').click(function (e) {
   e.preventDefault()
+  if($(this).html() === "Backlog") {
+	  $("div.page-header").html("<h1>Stoyboard - Backlog <small>Display the backlog tasks grouped by sprints</small></h1>");
+  } else {
+	  $("div.page-header").html("<h1>Stoyboard - Active Sprint <small>All the issues in the current sprint</small></h1>");
+  }
   $(this).tab('show')
 })
